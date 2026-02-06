412c604d4be5152626a8fa3b5ec87f34a0d8b5e2
ila <ilaria.battiston@gmail.com>
2026-02-06 09:55:42 +0100

    query fixes

--- benchmark/tpch_pac_queries/q04.sql
+++ benchmark/tpch_pac_queries/q04.sql
@@ -4,6 +4,6 @@ WHERE o_orderdate >= DATE '1993-07-01' AND o_orderdate <  DATE '1993-10-01'
   AND EXISTS (SELECT 1
               FROM lineitem
               WHERE l_orderkey = orders.o_orderkey
-                AND l_commitdate < l_receiptdate
+                AND l_commitdate < l_receiptdate)
 GROUP BY o_orderpriority
 ORDER BY o_orderpriority;
--- benchmark/tpch_pac_queries/q21.sql
+++ benchmark/tpch_pac_queries/q21.sql
@@ -5,11 +5,9 @@ FROM supplier JOIN lineitem l1 ON s_suppkey = l1.l_suppkey
 WHERE o_orderstatus = 'F'
   AND l1.l_receiptdate > l1.l_commitdate
   AND EXISTS (SELECT 1 FROM lineitem l2 JOIN orders o2 ON l2.l_orderkey = o2.o_orderkey
-                                        JOIN customer c2 ON o2.o_custkey = c2.c_custkey
               WHERE l2.l_orderkey = l1.l_orderkey AND l2.l_suppkey <> l1.l_suppkey)
   AND NOT EXISTS (SELECT 1 FROM lineitem l3 JOIN orders o3 ON l3.l_orderkey = o3.o_orderkey
-                                            JOIN customer c3 ON o3.o_custkey = c3.c_custkey
-                   WHERE l3.l_orderkey = l1.l_orderkey 
+                   WHERE l3.l_orderkey = l1.l_orderkey
                      AND l3.l_suppkey <> l1.l_suppkey
                      AND l3.l_receiptdate > l3.l_commitdate)
   AND n_name = 'SAUDI ARABIA'
