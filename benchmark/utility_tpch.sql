2f012c59480e51f3a25399f4c275c280ba65ab01
peter <boncz@cwi.nl>
2026-02-13 18:05:31 +0100

    add script to do utility testing for TPC-H (execute 100x, collect csv files)

---
+++ benchmark/utility_tpch.sql
@@ -0,0 +1,65 @@
+set pac_diffcols = '2:q01.csv';
+.read 'benchmark/tpch_pac_queries/q01.sql'
+
+set pac_diffcols = '1:q04.csv';
+.read 'benchmark/tpch_pac_queries/q04.sql'
+
+set pac_diffcols = '1:q05.csv';
+.read 'benchmark/tpch_pac_queries/q05.sql'
+
+set pac_diffcols = '0:q06.csv';
+.read 'benchmark/tpch_pac_queries/q06.sql'
+
+set pac_diffcols = '3:q07.csv';
+.read 'benchmark/tpch_pac_queries/q07.sql'
+
+set pac_diffcols = '1:q08.csv';
+.read 'benchmark/tpch_pac_queries/q08.sql'
+
+set pac_diffcols = '1:q08-nolambda.csv';
+.read 'benchmark/tpch_pac_queries/q08-nolambda.sql'
+
+set pac_diffcols = '2:q09.csv';
+.read 'benchmark/tpch_pac_queries/q09.sql'
+
+set pac_diffcols = '1:q11.csv';
+.read 'benchmark/tpch_pac_queries/q11.sql'
+
+set pac_diffcols = '1:q12.csv';
+.read 'benchmark/tpch_pac_queries/q12.sql'
+
+set pac_diffcols = '1:q13.csv';
+.read 'benchmark/tpch_pac_queries/q13.sql'
+
+set pac_diffcols = '0:q14.csv';
+.read 'benchmark/tpch_pac_queries/q14.sql'
+
+set pac_diffcols = '0:q14-nolambda.csv';
+.read 'benchmark/tpch_pac_queries/q14-nolambda.sql'
+
+set pac_diffcols = '1:q15.csv';
+.read 'benchmark/tpch_pac_queries/q15.sql'
+
+set pac_diffcols = '0:q17.csv';
+.read 'benchmark/tpch_pac_queries/q17.sql'
+
+set pac_diffcols = '0:q17-nolambda.csv';
+.read 'benchmark/tpch_pac_queries/q17-nolambda.sql'
+
+set pac_diffcols = '0:q19.csv';
+.read 'benchmark/tpch_pac_queries/q19.sql'
+
+set pac_diffcols = '1:q20.csv';
+.read 'benchmark/tpch_pac_queries/q20.sql'
+
+set pac_diffcols = '1:q20-nolambda.csv';
+.read 'benchmark/tpch_pac_queries/q20-nolambda.sql'
+
+set pac_diffcols = '1:q21.csv';
+.read 'benchmark/tpch_pac_queries/q21.sql'
+
+set pac_diffcols = '1:q22.csv';
+.read 'benchmark/tpch_pac_queries/q22.sql'
+
+set pac_diffcols = '1:q22-nolambda.csv';
+.read 'benchmark/tpch_pac_queries/q22-nolambda.sql'
